88f8db0c105c5f958a559f53598ff43757b1f422
BADIUDragos <dragos.badiu.a@hotmail.com>
2024-01-19 12:15:22 -0500

    api being served correctly

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", '127.0.0.1', "localhost:8080"]
+ALLOWED_HOSTS = ["localhost", '127.0.0.1', "api"]
 
 
 # Application definition
