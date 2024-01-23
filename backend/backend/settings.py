9cfe4037a97b19b21e238842d29575bebe50b977
BADIUDragos <dragos.badiu.a@hotmail.com>
2024-01-23 18:00:26 -0500

    some fixes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,9 +15,6 @@ from dotenv import load_dotenv
 
 import os.path
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
